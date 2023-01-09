7c9a54180c4d68681951d3dbdc7397005f2bdc0f
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-09 17:25:58 +0100

    Snippet text use props with composition API

--- src/components/SingPostView.vue
+++ src/components/SingPostView.vue
@@ -3,12 +3,22 @@
 <template>
   <div>
     <h3>{{ post.title }}</h3>
+    <p>{{ snippet }}</p>
   </div>
 </template>
 
 <script>
+import { computed } from "vue";
 export default {
   props: ["post"],
+  // Tack props as a first argument
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + "....";
+    });
+
+    return { snippet };
+  },
 };
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,8 +18,16 @@ export default {
 
   setup() {
     const posts = ref([
-      { title: "welcome to the blog", body: "Lorem ipsum", id: 1 },
-      { title: "top 5 css tips", body: "Lorem ipsum", id: 2 },
+      {
+        title: "welcome to the blog",
+        body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source",
+        id: 1,
+      },
+      {
+        title: "top 5 css tips",
+        body: "Lorem ipsum is not simply",
+        id: 2,
+      },
     ]);
     return { posts };
   },
