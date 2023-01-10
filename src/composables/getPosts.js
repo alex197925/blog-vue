5f613db013cc28a8ab8ab695607c34641d794afa
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-10 11:10:12 +0100

    Created composable folder with js file, put all logic of fetch posts data inside, importing inside of Home component

---
+++ src/composables/getPosts.js
@@ -0,0 +1,26 @@
+/** @format */
+import { ref } from "vue";
+
+const getPosts = () => {
+  const posts = ref([]);
+  const error = ref(null);
+
+  // Fetching data, don't forget call the function
+  const load = async () => {
+    try {
+      let data = await fetch("http://localhost:3000/posts");
+      // console.log(data);
+      // If is no data, display error message
+      if (!data.ok) {
+        throw Error("no data available");
+      }
+      posts.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+  return { posts, error, load };
+};
+
+export default getPosts;
