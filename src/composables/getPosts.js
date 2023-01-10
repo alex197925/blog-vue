863e008cc7823490d483284b76c6a1ce6ef82a25
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-10 15:31:05 +0100

    Removed timeout

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -9,9 +9,9 @@ const getPosts = () => {
   const load = async () => {
     try {
       // simulate delay
-      await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
-      });
+      // await new Promise((resolve) => {
+      //   setTimeout(resolve, 2000);
+      // });
       let data = await fetch("http://localhost:3000/posts");
       // console.log(data);
       // If is no data, display error message
