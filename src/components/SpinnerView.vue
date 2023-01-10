7a15edd58a8923c437aa8891bd1fc609a825e84d
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-10 13:24:03 +0100

    Added animation to spinner

--- src/components/SpinnerView.vue
+++ src/components/SpinnerView.vue
@@ -10,7 +10,15 @@
   width: 40px;
   height: 40px;
   margin: 30px auto;
+  border: 3px solid transparent;
   border-radius: 50%;
   border-top-color: #ff8800;
+  animation: spin 1s ease infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotateZ(360deg);
+  }
 }
 </style>
