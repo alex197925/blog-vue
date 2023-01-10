332a03f519668a7dd461f0654814446f2c7ba82d
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-10 15:31:48 +0100

    Added Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <!-- @format -->
 
 <template>
+  <NavbarView />
   <router-view />
 </template>
+
+<script>
+import NavbarView from "./components/NavbarView.vue";
+export default {
+  components: { NavbarView },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
