38e5422fcdea42dcba24306ae41270c75cdbb951
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-12 14:02:06 +0100

    Added Firebase to the project

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkZikZKE_EOxmZFquB3-ZjmUMW7emwfeE",
+  authDomain: "vue-firebase-sites-acd4b.firebaseapp.com",
+  projectId: "vue-firebase-sites-acd4b",
+  storageBucket: "vue-firebase-sites-acd4b.appspot.com",
+  messagingSenderId: "871812892803",
+  appId: "1:871812892803:web:34055b762871b15be8b3b6",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init firestore service
+const projectFirestore = firebase.firestore();
+
+// export firestore
+export { projectFirestore };
